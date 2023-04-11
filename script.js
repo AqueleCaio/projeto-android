89fa0373d9015bf9d05ff092fb6f591fc739ded4
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-04-11 17:03:44 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+function clickContent(){
+    if (hide_content.style.display == 'block'){
+        hide_content.style.display = 'none'
+    } else {
+        hide_content.style.display = 'block'
+    }
+}
