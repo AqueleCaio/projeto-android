db7d3ddbedf15beefacab732c703a827d298531f
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-04-14 16:58:42 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function clickContent(){
-    if (hide_content.style.display == 'block'){
+    if (hide_content.style.display == 'block') {
         hide_content.style.display = 'none'
     } else {
         hide_content.style.display = 'block'
+    }
+}
+
+function showMenu(){
+    if (imenu.style.display == 'block'){
+        imenu.style.display = 'none'
+    } else {
+        imenu.style.display = 'block'
     }
 }
