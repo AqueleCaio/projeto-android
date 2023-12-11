c211057475e8819ac5b2bd4efddf916dd1ca2b28
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-12-11 18:29:42 -0300

    código js convertido para jq

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-let op_content = document.getElementById('optinal_content')
-let content = document.getElementById('click_content')
-let icon_menu = document.getElementById('icon_menu')
-let content_menu = document.getElementById('imenu')
-let row = document.getElementById('icon_row')
+const op_content = document.getElementById('optinal_content')
 
-icon_menu.addEventListener('click', showMenu)
-content.addEventListener('click', clickContent)
-
-function clickContent(){
+$('#click_content').click(() =>{
     if (hide_content.style.display == 'block') {
-        hide_content.style.display = 'none'
+        (hide_content.style.display = 'none')
     } else {
-        hide_content.style.display = 'block'
+       (hide_content.style.display = 'block')
     }
     op_content.scrollIntoView({ behavior: "smooth" })
-}
+});
 
-/* ################################################## */
+/* ______________________________________________ */
 
-
-function showMenu(){
+$('#icon_menu').click(() =>{
     if (imenu.style.display == 'block'){
         imenu.style.display = 'none'
     } else {
         imenu.style.display = 'block'
     }
-}
+});
 
-/* ################################################## */
+/* ______________________________________________ */
 
 /* Função para mostrar o menu quando a tela for maior ou igual a 760px*/
-function keepMenu(){
+$(window).resize(() =>{
     if (window.innerWidth >= 760){
         imenu.style.display = 'block'
     } else {
         imenu.style.display = 'none'
     }
-}
+});
